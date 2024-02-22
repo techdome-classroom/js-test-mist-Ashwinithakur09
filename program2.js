80c8b47729f236044821419f4841a73dc7ac2983
Ashwinithakur09 <160574481+Ashwinithakur09@users.noreply.github.com>
2024-02-22 13:46:43 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,10 +1,14 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
+def romanToInt(s: str) -> int:
+    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
     
-};
-
-
-module.exports={romanToInt}
+    prev_value = 0
+    for char in reversed(s):
+        current_value = roman_values[char]
+        if current_value < prev_value:
+            result -= current_value
+        else:
+            result += current_value
+        prev_value = current_value
+    
+    return result
